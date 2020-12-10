bba275675bbc92bf9f2946808f705ec2b8e3181b
Ashley Oldershaw <ash.oldershaw@gmail.com>
2020-12-10 11:30:52 +0000

    adding extra comments to day 10

--- 10/10.py
+++ 10/10.py
@@ -1,20 +1,22 @@
 def get_joltage_differences(input_joltages):
+    # add socket and reader joltages
+    input_joltages.append(0)
+    input_joltages.append(max(input_joltages) + 3)
     input_joltages.sort()
 
+    # used to keep track of how many of each gap we have
+    # could use a list [0, 0, 0, 0] for this but this is clearer to read
     differences = {0: 0, 1: 0, 2: 0, 3: 0}
 
-    if input_joltages[0] <= 3:
-        differences[input_joltages[0]] += 1
-
     for i in range(1, len(input_joltages)):
+        # check we can actually do this jump (paranoia check)
         if 0 <= (diff := input_joltages[i] - input_joltages[i - 1]) <= 3:
             differences[diff] += 1
         else:
             print("Streak ended without using all adapters!")
             break
 
-    differences[3] += 1
-
+    # number needed for answer
     return differences[1] * differences[3]
 
 
@@ -24,13 +26,14 @@ def find_number_of_combinations(input_joltages):
     input_joltages.append(max(input_joltages) + 3)
     input_joltages.sort()
 
-    # the graph has all the links from it to other nodes, and stores how many paths it has to get to that node
+    # the graph has all the links from it to other nodes, and stores how many paths we have so far to get to the node
     graph = {i: {"links": [j for j in input_joltages if 0 < j - i <= 3], "paths": 0} for i in input_joltages}
 
     # add initial path, we add this to each link to graph[0] and add that to each link
     # when we reach a place where 2 paths can reach there then paths is added to twice
     # and so on until we get to the last node
-    # this is similar to Dijkstra's algorithm except we're just counting the no. of paths rather than the shortest path
+    # this is similar to Dijkstra's algorithm in terms of complexity
+    # except we're just counting the no. of paths rather than the shortest path
     graph[0]["paths"] = 1
     for node in sorted(graph.keys()):
         for link in graph[node]["links"]:
